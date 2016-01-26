cb6d7fe7323427107bd88e8860b651521408e556
djgonza1 <djgonza1@uci.edu>
2016-01-26 10:59:49 -0800

    asdfasfdfsaf

--- src/ir/assignments/three/Crawler.java
+++ src/ir/assignments/three/Crawler.java
@@ -1,8 +1,14 @@
 package ir.assignments.three;
 
 import java.util.Collection;
+import java.util.Set;
 
-public class Crawler {
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class Crawler extends WebCrawler {
 	/**
 	 * This method is for testing purposes only. It does not need to be used
 	 * to answer any of the questions in the assignment. However, it must
@@ -13,6 +19,36 @@ public class Crawler {
 	 */
 	public static Collection<String> crawl(String seedURL) {
 		// TODO implement me
+		
+		
+		
 		return null;
 	}
+	
+	@Override
+	public void visit(Page page) {
+	    String url = page.getWebURL().getURL();
+	    System.out.println("\nURL: " + url);
+	    System.out.println("ID: " + url.toString().hashCode());
+	
+	    if (page.getParseData() instanceof HtmlParseData) {
+	        HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+	        String text = htmlParseData.getText();
+	        String html = htmlParseData.getHtml();
+	        Set<WebURL> links = htmlParseData.getOutgoingUrls();
+	
+	        System.out.println("Text length: " + text.length());
+	        System.out.println("Html length: " + html.length());
+	        System.out.println("Number of outgoing links: " + links.size());
+	    }
+	}
+	
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+	    String href = url.getURL().toLowerCase();
+	    // Only accept the url if it is in the "www.ics.uci.edu" domain and protocol is "http".
+	    return href.startsWith("http://www.ics.uci.edu/");
+	    
+	}
+	
 }
---
+++ src/ir/assignments/three/CrawlerController.java
@@ -0,0 +1,52 @@
+package ir.assignments.three;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import ir.assignments.three.*;
+
+public class CrawlerController {
+	
+	public static void main(String[] args) {
+		String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setUserAgentString("David&HarryTestCrawler");
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        
+        try{
+        	CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        	/*
+             * For each crawl, you need to add some seed urls. These are the first
+             * URLs that are fetched and then the crawler starts following links
+             * which are found in these pages
+             */
+            controller.addSeed("http://www.ics.uci.edu/~lopes/");
+            controller.addSeed("http://www.ics.uci.edu/~welling/");
+            controller.addSeed("http://www.ics.uci.edu/");
+
+            /*
+             * Start the crawl. This is a blocking operation, meaning that your code
+             * will reach the line after this only when crawling is finished.
+             */
+            controller.start(Crawler.class, numberOfCrawlers);
+            
+        }
+        catch(Exception e)
+        {
+        	
+        };
+
+        
+	}
+}
